186b753a9370dd56fd3a48a4f797ca147f9c43e6
Mungujakisa Nickson <nicksonmjk@gmail.com>
2018-09-14 20:49:55 +0300

    fixed classifier threading

--- classifier.py
+++ classifier.py
@@ -1,6 +1,7 @@
 from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import image
 from keras.applications.resnet50 import preprocess_input, decode_predictions
+import tensorflow as tf
 import numpy as np
 import os
 model = None
@@ -9,20 +10,20 @@ model_loaded = False
 
 def loadModel():
     global model_loaded
-    model_loaded = True
-    
+
     if not model_loaded:
         global model
         model = ResNet50(weights='imagenet')
-        
+        global graph
+        # this will allow the model to be called from a different thread than the defining thread
+        graph = tf.get_default_graph()
+        model_loaded = True
 
 
 def predictImageClass(folder, image_file_name):
-    """Predicts the likely class of an image
+    """Returns a string with the top five most likely classes of an image
     """
-    if not model_loaded:
-        loadModel()
-
+    global model
     path = os.path.join('..', 'files', folder, image_file_name)
 
     img = image.load_img(path, target_size=(224, 224))
@@ -30,7 +31,13 @@ def predictImageClass(folder, image_file_name):
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
 
-    predictions = model.predict(x)
-    predictions = decode_predictions(predictions, top=1)[0]
-    # print('Predicted: ', predictions)
-    return predictions[0][1]
+    # allow the model to be called from a different thread than the defining thread
+    with graph.as_default():
+        predictions = model.predict(x)
+
+    predictions = decode_predictions(predictions, top=5)[0]
+
+    top_five = ''
+    for prediction in predictions:
+        top_five += f' {prediction[1]}'
+    return top_five
